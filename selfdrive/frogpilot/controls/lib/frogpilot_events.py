53ca741db847bbe31a000e610d88b6e3e3bc357f
FrogAi <91348155+FrogAi@users.noreply.github.com>
2025-01-01 23:47:29 -0700

    January 1st, 2025 Update

--- selfdrive/frogpilot/controls/lib/frogpilot_events.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_events.py
@@ -61,7 +61,7 @@ class FrogPilotEvents:
       self.stopped_for_light = False
 
     if not self.holiday_theme_played and frogpilot_toggles.current_holiday_theme != "stock" and self.frame >= 10:
-      #self.events.add(EventName.holidayActive)
+      self.events.add(EventName.holidayActive)
       self.holiday_theme_played = True
 
     if frogpilot_toggles.lead_departing_alert and self.frogpilot_planner.tracking_lead and carState.standstill:
@@ -177,7 +177,7 @@ class FrogPilotEvents:
           self.random_event_played = True
       self.always_on_lateral_active_previously = frogpilotCarControl.alwaysOnLateralActive
 
-    if frogpilot_toggles.speed_limit_changed_alert and self.frogpilot_planner.frogpilot_vcruise.speed_limit_changed:
+    if frogpilot_toggles.speed_limit_changed_alert and self.frogpilot_planner.frogpilot_vcruise.speed_limit_changed and self.frogpilot_planner.frogpilot_vcruise.speed_limit_timer < 1:
       self.events.add(EventName.speedLimitChanged)
 
     if 5 > self.frame > 4 and params.get("NNFFModelName", encoding='utf-8') is not None:
