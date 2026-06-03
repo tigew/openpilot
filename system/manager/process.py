9de041e118e3af246b677d3ea081b77bf785dbb1
James <91348155+FrogAi@users.noreply.github.com>
2026-06-03 02:06:20 -0700

    June 3rd, 2026 Update

--- system/manager/process.py
+++ system/manager/process.py
@@ -141,12 +141,6 @@ class ManagerProcess(ABC):
 
     return ret
 
-  def reap_if_dead(self) -> None:
-    if self.proc is not None and self.proc.exitcode is not None:
-      cloudlog.info(f"{self.name} is dead with {self.proc.exitcode}, restarting")
-      self.shutting_down = False
-      self.proc = None
-
   def signal(self, sig: int) -> None:
     if self.proc is None:
       return
@@ -192,8 +186,6 @@ class NativeProcess(ManagerProcess):
     if self.shutting_down:
       self.stop()
 
-    self.reap_if_dead()
-
     if self.proc is not None:
       return
 
@@ -224,8 +216,6 @@ class PythonProcess(ManagerProcess):
     # In case we only tried a non blocking stop we need to stop it before restarting
     if self.shutting_down:
       self.stop()
-
-    self.reap_if_dead()
 
     if self.proc is not None:
       return
